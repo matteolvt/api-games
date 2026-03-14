fdb63fbd4cca1ab3e981687381e6e2b0fff5a1c9
matteolvt <146728526+matteolvt@users.noreply.github.com>
2026-03-14 17:19:27 +0100

    fix: add node globals to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,17 @@ const js = require('@eslint/js')
 module.exports = [
   js.configs.recommended,
   {
+    languageOptions: {
+      globals: {
+        require: 'readonly',
+        module: 'readonly',
+        exports: 'readonly',
+        process: 'readonly',
+        console: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      }
+    },
     rules: {
       'no-unused-vars': 'warn',
       'no-console': 'off'
